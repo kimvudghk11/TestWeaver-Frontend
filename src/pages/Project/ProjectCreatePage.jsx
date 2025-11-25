c8991ae9ad8889078ec63edbd5504244a760c959
KimSnowFlower <kimbona1148@gmail.com>
2025-11-25 18:31:05 +0900

    feat: #23 complete UI design for Project Create Page

--- src/pages/Project/ProjectCreatePage.jsx
+++ src/pages/Project/ProjectCreatePage.jsx
@@ -1,21 +1,71 @@
 import MainLayout from "../../components/Layout/MainLayout";
+import Input from "../../components/UI/Input";
 import Button from "../../components/UI/Button";
 import CircleVerticalBar from "../../components/UI/CircleVerticalBar";
-import styles from "./Project.module.css";
+import styles from "./ProjectCreate.module.css";
 
 export default function ProjectCreatePage() {
     return (
         <MainLayout>
-            <div className={styles.dashboard}>
+            <div className={styles.container}>
                 <div className={styles.header}>
-                    <div className={styles.headerLeft}>
-                        <h1 className={styles.title}>Create New Test Case</h1>
-                        <p className={styles.subtitle}>Select your new project</p>
+                    <h1 className={styles.title}>Create New Test Case</h1>
+                    <p className={styles.subtitle}>Select your new project</p>
+                </div>
+
+                <div className={styles.inputWrapper}>
+                    <CircleVerticalBar text="1" color="#393E46" barHeight={220} />
+
+                    <div className={styles.general}>
+                        <div className={styles.inputTitle}>General</div>
+
+                        <Input label="Project Name *" placeholder="input your project Name" />
+                        <Input label="Description" placeholder="input your project description" />
                     </div>
                 </div>
 
-                <CircleVerticalBar text="1" color="#393E46" barHeight={120} />
+                <div className={styles.inputWrapper}>
+                    <CircleVerticalBar text="2" color="#393E46" barHeight={250} />
+
+                    <div className={styles.general}>
+                        <div className={styles.inputTitle}>Configuration</div>
+
+                        {/* Strategy */}
+                        <div className={styles.radioGroupWrapper}>
+                            <div className={styles.radioTitle}>Strategy</div>
+
+                            <div className={styles.radioGroup}>
+                                <label>
+                                    <input type="radio" name="strategy" value="IPO" defaultChecked />
+                                    IPO
+                                </label>
+                                <label>
+                                    <input type="radio" name="strategy" value="IPOG" />
+                                    IPOG
+                                </label>
+                            </div>
+                        </div>
+
+                        {/* Test Coverage */}
+                        <div className={styles.radioGroupWrapper}>
+                            <div className={styles.radioTitle}>Test Coverage</div>
+
+                            <div className={styles.radioGroup}>
+                                <label>
+                                    <input type="radio" name="coverage" value="2-way" defaultChecked />
+                                    2-way
+                                </label>
+                                <label>
+                                    <input type="radio" name="coverage" value="3-way" />
+                                    3-way
+                                </label>
+                            </div>
+                        </div>
+
+                        <Button type="submit">Create Test Case</Button>
+                    </div>
+                </div>
             </div>
-        </MainLayout>
+        </MainLayout >
     );
 }
