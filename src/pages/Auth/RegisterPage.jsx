28686903a5a4908f0ddaaf0c1b7d856da39fbc56
KimSnowFlower <kimbona1148@gmail.com>
2025-11-25 22:31:03 +0900

    feat: #29 connect Register and ResetPassword Pages to API

--- src/pages/Auth/RegisterPage.jsx
+++ src/pages/Auth/RegisterPage.jsx
@@ -1,17 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { authApi } from "../../api/authApi";
+
 import AuthLayout from "../../components/Layout/AuthLayout";
 import Input from "../../components/UI/Input";
 import Button from "../../components/UI/Button";
 import styles from "./Auth.module.css";
 
 export default function RegisterPage() {
+    const navigate = useNavigate();
+
+    const [form, setForm] = useState({
+        loginId: "",
+        email: "",
+        name: "",
+        password: "",
+        phone: "",
+    });
+
+    const onChange = (e) =>
+        setForm({ ...form, [e.target.name]: e.target.value });
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await authApi.register(form);
+
+            navigate("/auth/login");
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
     return (
         <AuthLayout title="Sing up">
-            <form className={styles.form}>
-                <Input label="User Id" type="text" />
-                <Input label="Email" type="email" />
-                <Input label="Name" type="text" />
-                <Input label="Password" type="password" />
-                <Input label="Phone" type="text" />
+            <form className={styles.form} onSubmit={onSubmit}>
+                <Input name="loginId" label="User Id" placeholder="EX) testweaver" type="text" onChange={onChange} />
+                <Input name="email" label="Email" placeholder="EX) testweaver@testweaver.com" type="email" onChange={onChange} />
+                <Input name="name" label="Name" placeholder="EX) kim testweaver" type="text" onChange={onChange} />
+                <Input name="password" label="Password" placeholder="EX)testweaver password" type="password" onChange={onChange} />
+                <Input name="phone" label="Phone" placeholder="EX) xxx-xxxx-xxxx" type="text" onChange={onChange} />
+
                 <Button type="submit">Sing up</Button>
             </form>
         </AuthLayout>
--- src/pages/Auth/ResetPwPage.jsx
+++ src/pages/Auth/ResetPwPage.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { authApi } from "../../api/authApi";
+
 import AuthLayout from "../../components/Layout/AuthLayout";
 import Input from "../../components/UI/Input";
 import Button from "../../components/UI/Button";
 import styles from "./Auth.module.css";
 
 export default function RestPwPage() {
+    const navigate = useNavigate();
+
+    const [form, setForm] = useState({
+        loginId: "",
+        name: "",
+        email: "",
+        newPassword: "",
+    });
+
+    const onChange = (e) =>
+        setForm({ ...form, [e.target.name]: e.target.value });
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await authApi.resetPassword(form);
+
+            navigate("/auth/login");
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
     return (
         <AuthLayout title="Reset Password">
-            <form className={styles.form}>
-                <Input label="User Name" type="text" />
-                <Input label="User Email" type="email" />
+            <form className={styles.form} onSubmit={onSubmit}>
+                <Input name="loginId" label="User ID" placeholder="your ID" type="text" onChange={onChange} />
+                <Input name="name" label="User Name" placeholder="your Name" type="text" onChange={onChange} />
+                <Input name="email" label="User Email" placeholder="your Email" type="email" onChange={onChange} />
+                <Input name="newPassword" label="New Password" placeholder="your New Password" type="password" onChange={onChange} />
+
                 <Button type="submit">Change Password</Button>
             </form>
         </AuthLayout>
